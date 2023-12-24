4cba48fa12f1ef17d42fe62c7277e64eedd5fa8b
Ayoub <youbima17@gmail.com>
2023-12-24 15:25:35 +0100

    clear

--- kernel/src/main/java/fr/teama/structural/classic/ClassicNote.java
+++ kernel/src/main/java/fr/teama/structural/classic/ClassicNote.java
@@ -1,7 +1,6 @@
 package fr.teama.structural.classic;
 
 import fr.teama.generator.Visitable;
-import fr.teama.generator.Visitor;
 import fr.teama.structural.abstracts.Note;
 import fr.teama.structural.enums.NoteDurationEnum;
 import fr.teama.structural.enums.ClassicNoteEnum;
@@ -10,9 +9,5 @@ public class ClassicNote extends Note implements Visitable {
 
     public ClassicNote(ClassicNoteEnum classicNoteEnum, NoteDurationEnum noteDurationEnum) {
         super(classicNoteEnum, noteDurationEnum);
-    }
-
-    public void accept(Visitor visitor) {
-        visitor.visit(this);
     }
 }
--- kernel/src/main/java/fr/teama/structural/classic/ClassicTrack.java
+++ kernel/src/main/java/fr/teama/structural/classic/ClassicTrack.java
@@ -1,7 +1,6 @@
 package fr.teama.structural.classic;
 
 import fr.teama.generator.Visitable;
-import fr.teama.generator.Visitor;
 import fr.teama.structural.abstracts.Track;
 
 public class ClassicTrack extends Track implements Visitable {
