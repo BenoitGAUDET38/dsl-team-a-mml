1021f10dce4fd43b89d11e54960165b40af76189
Ayoub <youbima17@gmail.com>
2023-12-24 15:21:05 +0100

    Drum note

--- kernel/src/main/java/fr/teama/structural/drum/DrumNote.java
+++ kernel/src/main/java/fr/teama/structural/drum/DrumNote.java
@@ -1,4 +1,13 @@
 package fr.teama.structural.drum;
 
-public class DrumNote {
+import fr.teama.generator.Visitable;
+import fr.teama.structural.abstracts.Note;
+import fr.teama.structural.enums.DrumNoteEnum;
+import fr.teama.structural.enums.NoteDurationEnum;
+
+public class DrumNote extends Note implements Visitable {
+
+    public DrumNote(DrumNoteEnum drumNoteEnum, NoteDurationEnum noteDurationEnum) {
+        super(drumNoteEnum, noteDurationEnum);
+    }
 }
