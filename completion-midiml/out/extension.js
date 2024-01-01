966458728404e3da965a908940d87374c4336f88
Benoit Gaudet <bgaudet38@gmail.com>
2024-01-01 19:15:47 +0100

    Setup of completion extension

---
+++ completion-midiml/out/extension.js
@@ -0,0 +1,92 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = __importStar(require("vscode"));
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+function activate(context) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "identifier-test" is now active!');
+    const provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
+        provideCompletionItems(document, position, token, context) {
+            // a simple completion item which inserts `Hello World!`
+            const simpleCompletion = new vscode.CompletionItem('Hello World!');
+            // a completion item that inserts its text as snippet,
+            // the `insertText`-property is a `SnippetString` which will be
+            // honored by the editor.
+            const snippetCompletion = new vscode.CompletionItem('Good part of the day');
+            snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
+            const docs = new vscode.MarkdownString("Inserts a snippet that lets you select [link](x.ts).");
+            snippetCompletion.documentation = docs;
+            docs.baseUri = vscode.Uri.parse('http://example.com/a/b/c/');
+            // a completion item that can be accepted by a commit character,
+            // the `commitCharacters`-property is set which means that the completion will
+            // be inserted and then the character will be typed.
+            const commitCharacterCompletion = new vscode.CompletionItem('instrument');
+            commitCharacterCompletion.commitCharacters = [' '];
+            commitCharacterCompletion.documentation = new vscode.MarkdownString('Press ` ` to get `liste of instruments`');
+            // a completion item that retriggers IntelliSense when being accepted,
+            // the `command`-property is set which the editor will execute after 
+            // completion has been inserted. Also, the `insertText` is set so that 
+            // a space is inserted after `new`
+            const commandCompletion = new vscode.CompletionItem('new');
+            commandCompletion.kind = vscode.CompletionItemKind.Keyword;
+            commandCompletion.insertText = 'new ';
+            commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
+            // return all completion items as array
+            return [
+                simpleCompletion,
+                snippetCompletion,
+                commitCharacterCompletion,
+                commandCompletion
+            ];
+        }
+    });
+    const provider2 = vscode.languages.registerCompletionItemProvider('plaintext', {
+        provideCompletionItems(document, position) {
+            // get all text until the `position` and check if it reads `console.`
+            // and if so then complete if `log`, `warn`, and `error`
+            const linePrefix = document.lineAt(position).text.slice(0, position.character);
+            if (!linePrefix.endsWith('instrument ')) {
+                return undefined;
+            }
+            return [
+                new vscode.CompletionItem('PIANO', vscode.CompletionItemKind.Method),
+                new vscode.CompletionItem('BATTERIE', vscode.CompletionItemKind.Method)
+            ];
+        }
+    }, ' ' // triggered whenever a '.' is being typed
+    );
+    context.subscriptions.push(provider1, provider2);
+}
+exports.activate = activate;
+// This method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
