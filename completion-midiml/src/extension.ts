eb1a46b4156c67249650ade6dbbaa958ea996dfa
Benoit Gaudet <bgaudet38@gmail.com>
2024-01-08 11:29:14 +0100

    Extension regex modification

--- completion-midiml/src/extension.ts
+++ completion-midiml/src/extension.ts
@@ -4,6 +4,9 @@ import * as vscode from 'vscode';
 
 const notes = ['DO', 'DO_D', 'RE', 'RE_D', 'MI', 'FA', 'FA_D', 'SOL', 'SOL_D', 'LA', 'LA_D', 'SI'];
 const noteDuration = ['D_C', 'C', 'C_P', 'P', 'N', 'N_P', 'BL', 'BL_P', 'R'];
+const instrumentOptions = ['PIANO', 'BATTERIE', 'XYLOPHONE', 'ACCORDEON', 'HARMONICA', 'GUITARE', 'CONTREBASSE', 'VIOLON', 'TROMPETTE', 'TROMBONE', 'ALTO', 'CLARINETTE', 'FLUTE', 'WHISTLE', 'OCARINA', 'BANJO'];
+const rythmeOptions = ['3/4', '4/4', '6/8', '7/8'];
+const tempoOptions = ['80 bpm', '100 bpm', '120 bpm'];
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -18,12 +21,6 @@ export function activate(context: vscode.ExtensionContext) {
         {
 
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-
-			// a completion item that inserts its text as snippet
-			const rythmeOptions = ['3/4', '4/4', '6/8', '7/8'];
-			const tempoOptions = ['80 bpm', '100 bpm', '120 bpm'];
-			const instrumentOptions = ['PIANO', 'BATTERIE', 'XYLOPHONE', 'ACCORDEON', 'HARMONICA', 'GUITARE', 'CONTREBASSE', 'VIOLON', 'TROMPETTE', 'TROMBONE', 'ALTO', 'CLARINETTE', 'FLUTE', 'WHISTLE', 'OCARINA', 'BANJO'];
-
             // Snippet that will be inserted
 			const snippetContent = `titre default-title\n\n# Declaring settings\ntempo \${1|${tempoOptions.join(',')}\|} rythme \${2|${rythmeOptions.join(',')}\|}\n\ninstrument \${3|${instrumentOptions.join(',')}\|}\n{\n    \n}\n`;
 
@@ -71,27 +68,16 @@ export function activate(context: vscode.ExtensionContext) {
 				// and if so then complete if `log`, `warn`, and `error`
 				const linePrefix = document.lineAt(position).text.slice(0, position.character);
 				if (linePrefix.endsWith('instrument ')) {
-					return [
-                        new vscode.CompletionItem('PIANO', vscode.CompletionItemKind.Method),
-                        new vscode.CompletionItem('BATTERIE', vscode.CompletionItemKind.Method)
-                    ];
+                    return instrumentOptions.map(instrument => new vscode.CompletionItem(instrument, vscode.CompletionItemKind.Method));
 				} else if (linePrefix.endsWith('rythme ')) {
-                    return [
-                        new vscode.CompletionItem('3/4', vscode.CompletionItemKind.Method),
-                        new vscode.CompletionItem('4/4', vscode.CompletionItemKind.Method),
-                        new vscode.CompletionItem('6/8', vscode.CompletionItemKind.Method)
-                    ];
+                    return rythmeOptions.map(rythme => new vscode.CompletionItem(rythme, vscode.CompletionItemKind.Method));
                 } else if (linePrefix.endsWith('tempo ')) {
-                    return [
-                        new vscode.CompletionItem('80 bpm', vscode.CompletionItemKind.Method),
-                        new vscode.CompletionItem('100 bpm', vscode.CompletionItemKind.Method),
-                        new vscode.CompletionItem('120 bpm', vscode.CompletionItemKind.Method)
-                    ];
-                } else if (linePrefix.endsWith('| ')) {
+                    return tempoOptions.map(tempo => new vscode.CompletionItem(tempo, vscode.CompletionItemKind.Method));
+                } else if (linePrefix.endsWith('| ') || linePrefix.match(/.*(DO|DO_D|RE|RE_D|MI|FA|FA_D|SOL|SOL_D|LA|LA_D|SI|SILENCE)(-2|-1|0|1|2|3|4|5|6|7)?[ ]/)) {
                     const notesCompletion = notes.map(note => new vscode.CompletionItem(note, vscode.CompletionItemKind.Method));
                     notesCompletion.push(new vscode.CompletionItem('SILENCE', vscode.CompletionItemKind.Method));
                     return notesCompletion;
-                } else if (linePrefix.endsWith('SILENCE ') || linePrefix.endsWith('D_C ') || linePrefix.endsWith('C ') || linePrefix.endsWith('C_P ') || linePrefix.endsWith('P ') || linePrefix.endsWith('N ') || linePrefix.endsWith('N_P ') || linePrefix.endsWith('BL ') || linePrefix.endsWith('BL_P ') || linePrefix.endsWith('R ')) {
+                } else if (linePrefix.match(/(SILENCE|D_C|C|C_P|P|N|N_P|BL|BL_P|R)[ ]/)) {
                     const notesCompletion = notes.map(note => new vscode.CompletionItem(note, vscode.CompletionItemKind.Method));
                     notesCompletion.push(new vscode.CompletionItem('SILENCE', vscode.CompletionItemKind.Method));
                     notesCompletion.push(new vscode.CompletionItem('|', vscode.CompletionItemKind.Method));
